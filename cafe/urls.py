5a643d463195b1bd6dd36fa490d41cf056de7cc5
SwastiKh <swastikhurana0800@gmail.com>
2020-05-29 13:28:59 +0530

    categories for meals added

--- cafe/urls.py
+++ cafe/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('meals/', include('meals.urls', namespace='meals')),
+    path('reserve_table/', include('reservation.urls', namespace='reservation')),
 ]
 
 
